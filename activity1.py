079080f0cb9e16dfafe2e3cc0c5fdc230af33741
Aiden-2010 <aidenmartin.mgs@gmail.com>
2025-09-30 19:46:32 +0100

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,11 @@
+num1 = 6
+print("Type of num1: ", type(num1))
+
+num2 = 3.5
+print("Type of num2: ", type(num2))
+
+str1 = "Hello"
+print("Type of str1: ", type(str1))
+
+b = True
+print("Type of b: ", type(b))
---
+++ activity3.py
@@ -0,0 +1,8 @@
+txt = str(input("Enter a string: "))
+
+#reverse the string
+
+revTxt = txt[::-1]
+txt = revTxt
+print("Reverse of the given string is: ")
+print(txt)
